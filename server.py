63baaebfea9069291f2c095f1d876e0f778922e4
ddy123 <2440302702@qq.com>
2025-11-18 21:41:54 +0800

    upload test

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,send_file
 import os
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import cv2
+import numpy as np
+import io
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
@@ -16,18 +22,25 @@ def gettest():
 
 @app.route('/posttest', methods=['POST'])
 def posttest():
-    print(request.method+'\n')
-    print(request.url+'\n')
-    print(request.args.get('name')+'\n')
-    return request.args.get('name')
+    return "ok"
 
 @app.route('/detect_objects', methods=['POST'])
 def detect_objects():
     print(request.method+'\n')
     print(request.url+'\n')
-    print(request.args.get('name')+'\n')
-    return request.args.get('name')
-
+    image_data = request.get_data()
+    #print(image_data)
+    """  # 解码图像
+    nparr = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imshow('YOLO物体检测 - 气味识别', image)  """
+    print(request.headers.get('X-File-Name'))
+    # 创建内存中的图片文件 
+    img_io = io.BytesIO(image_data)
+    # 直接返回图片给浏览器显示
+    return send_file(img_io, mimetype='image/jpeg')
+    #return "ok"
+    #return jsonify({"success": True}),200
 
 if __name__ == '__main__':
     #app.run(debug=True)
